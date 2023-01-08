a60fab67aea6b6dcd6ae6e50b9675ee46b707e6d
Sandor Semsey <semseysandor@gmail.com>
2023-01-08 18:11:54 +0100

    remove unsupported platforms

--- Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Mortee;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
